19ca9cf0cc82a73765a0064c6ba2b2b1cd7ce890
Mohamed Zied Maaloul <med.zied.maaloul@gmail.com>
2023-11-17 20:37:29 +0100

    deploy-symfony-render

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,18 +6,48 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('libelle')
-            ->add('image')
-            ->add('quantity')
-            ->add('price')
-            ->add('fournisseur')
-        ;
+        $builder->add('libelle', TextType::class, [
+            'label' => 'Text',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('image', FileType::class, [
+            'label' => 'File Upload',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('quantity', NumberType::class, [
+            'label' => 'Number',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('price', NumberType::class, [
+            'label' => 'Number',
+            'attr' => ['class' => 'form-control'],
+        ])
+        ->add('fournisseur', ChoiceType::class, [
+            'label' => 'Select',
+            'choices' => [
+                'Option 1' => 'option1',
+                'Option 2' => 'option2',
+                'Option 3' => 'option3',
+            ],
+            'attr' => ['class' => 'form-select'],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
