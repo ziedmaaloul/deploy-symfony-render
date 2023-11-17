64eb8eda87197b4aca19e5b4a9ead1f3c0f5b519
Mohamed Zied Maaloul <med.zied.maaloul@gmail.com>
2023-11-18 00:03:32 +0100

    deploy-symfony-render

--- src/Form/CommandeType.php
+++ src/Form/CommandeType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use App\Repository\FournisseurRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CommandeType extends AbstractType
 {
