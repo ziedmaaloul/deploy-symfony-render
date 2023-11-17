6adc9c210b03572382aa18a6b5d97efdcdac67ca
Mohamed Zied Maaloul <med.zied.maaloul@gmail.com>
2023-11-17 22:19:43 +0100

    deploy-symfony-render

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -17,12 +17,22 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Repository\FournisseurRepository;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProduitType extends AbstractType
 {
+
+    private $fournisseurRepository = null;
+    public function __construct(FournisseurRepository $fournisseurRepository)
+    {
+        $this->fournisseurRepository = $fournisseurRepository;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        dd($this->fournisseurRepository->findAll());
         $builder->add('libelle', TextType::class, [
             'label' => 'Nom de produit',
             'attr' => ['class' => 'form-control'],
